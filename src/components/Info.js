08fad9a4adb7d670a01e1814a5995e5ec019459f
akulikov <akulikov@avaya.com>
2019-05-20 17:48:13 +0300

    small addings

---
+++ src/components/Info.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Info extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h2>Weather app</h2>
+                <p>Discover the weather in your town</p>
+            </div>
+        );
+    }
+
+}
+
+export default Info;
